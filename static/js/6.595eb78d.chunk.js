606837ec03e4b7ab55c6123e2df81bbd8c546bfa
rbirney <rbirney@wit.ie>
2021-04-27 07:52:19 +0100

    first build pizzapal

---
+++ static/js/6.595eb78d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppizzapal=this.webpackJsonppizzapal||[]).push([[6],{225:function(e,r,c){"use strict";c.r(r);c(0);var s=c(196),a=c(104),i=c(146),t=c(2);r.default=function(e){return Object(t.jsxs)(s.a,{color:"red",children:[Object(t.jsx)(a.a,{as:"h2",textAlign:"center",color:"red",children:"Your Order has been placed!"}),Object(t.jsx)(i.a,{src:"/images/delivery.gif",size:"medium",centered:!0})]})}}}]);
+//# sourceMappingURL=6.595eb78d.chunk.js.map
